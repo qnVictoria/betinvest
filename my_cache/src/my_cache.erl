cabfa8d831072c160b4fcb5e6447d2aac512b986
Victoria Romaniuk <victoria7maksimenko@gmail.com>
2020-12-09 11:17:52 +0200

    fix task 1 lection 5

--- my_cache/src/my_cache.erl
+++ my_cache/src/my_cache.erl
@@ -3,23 +3,51 @@
 -export([create/1, insert/4, lookup/2, delete_obsolete/1]).
 
 create(TableName) ->
-  ets:new(TableName, [public, named_table, set]),
-  ok.
+  case is_present(TableName) of
+    true ->
+      table_is_present;
+    false ->
+      ets:new(TableName, [public, named_table, set]),
+      ok
+  end.
 
 insert(TableName, Key, Value, TTL) when is_integer(TTL) ->
-  ets:insert(TableName, {Key, Value, os:system_time(second) + TTL}),
-  ok;
+  case is_present(TableName) of
+    true ->
+      ets:insert(TableName, {Key, Value, os:system_time(second) + TTL}),
+      ok;
+    false ->
+      error
+  end;
 
 insert(TableName, Key, Value, TTL) ->
   error.
 
 lookup(TableName, Key) ->
-  [{Key, Value, EndTime}] = ets:lookup(TableName, Key),
-  case EndTime > os:system_time(second) of
-    true -> {ok, Value};
-    false -> error
+  case is_present(TableName) of
+    true ->
+      case ets:member(TableName, Key) of
+        true ->
+          [{Key, Value, EndTime}] = ets:lookup(TableName, Key),
+          case EndTime > os:system_time(second) of
+            true -> {ok, Value};
+            false -> {error, not_found}
+          end;
+        false ->
+          {error, not_found}
+      end;
+    false ->
+      error
   end.
 
 delete_obsolete(TableName) ->
-  ets:select_delete(TableName, [{{'$1','$2','$3'},[{'<','$3', os:system_time(second)}],[true]}]),
-  ok.
+  case is_present(TableName) of
+    true ->
+      ets:select_delete(TableName, [{{'$1','$2','$3'},[{'<','$3', os:system_time(second)}],[true]}]),
+      ok;
+    false ->
+      error
+  end.
+
+is_present(TableName) ->
+  lists:member(TableName, ets:all()).
